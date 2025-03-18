e6db46504572c72bdb96cf9655fbb82064da9987
Paulos Gudu <54817437+paulgitmum@users.noreply.github.com>
2025-03-17 20:09:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 
+const express = require("express");
+const escapeHtml = require("escape-html"); 
+const app = express();
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`
+        <form action="/search" method="GET">
+            <input type="text" name="query" placeholder="Enter your search">
+            <button type="submit">Search</button>
+        </form>
+    `);
+});
+
+app.get("/search", (req, res) => {
+    const query = req.query.query;
+    
+    // Simulated SQL injection vulnerability 
+    const sqlQuery = `SELECT * FROM users WHERE name = '${query}'`; 
+
+    // XSS vulnerability remains
+    res.send(`<h1>Results for: ${query}</h1>`); 
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
